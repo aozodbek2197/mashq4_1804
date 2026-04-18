f931478f5d1e938bbbeb3a45d7a22b504862afc3
aozodbek2197 <aozodbek2197@gmail.com>
2026-04-18 12:35:06 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# 1-mashq
+nums = list(map(int, input().split()))
+print(len(nums))
+# 2-mashq
+nums = list(map(int, input().split()))
+print(len(nums))
+# 3-mashq
+nums = list(map(int, input().split()))
+print(min(nums))
+# 4-mashq
+nums = list(map(int, input().split()))
+print(nums[::-1])
+# 5-mashq
+nums = list(map(int, input().split()))
+x = int(input())
+print(x in nums)
